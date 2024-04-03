874c37fda7d10f2b19ff7c73732a4bf20c23a4a7
Steven Zheng <szheng.persevere@gmail.com>
2024-04-03 11:10:17 -0500

    gave the server a way to test for whether the date is unix or not

--- routes/dateRouter.js
+++ routes/dateRouter.js
@@ -3,18 +3,31 @@ const router = express.Router()
 
 router.get("/:date?",(req,res)=>{
     const {date}=req.params
-    console.log(date)
     if(!date){
         var newDate = new Date()
         let unix = Date.now()
         let utc = newDate.toUTCString()
-        console.log(unix,utc)
         res.json({unix:unix,utc:utc})
-    } else {
-        let utc = new Date(date).toUTCString()
-        let unix = new Date(date).getTime()
-        console.log(unix,utc)
-        res.json({unix:unix,utc:utc})
+    } else{
+        let testArr = "1234567890"
+        let isUnix =true
+        for(let num of date){
+            if(!testArr.includes(num)){
+                isUnix = false
+            }
+        }
+        console.log(isUnix)
+        if(!isUnix){
+            let utc = new Date(date).toUTCString()
+            let unix = new Date(date).getTime()
+            console.log(unix,utc)
+            res.json({unix:unix,utc:utc})
+        } else {
+            let utc = new Date(Number(date)).toUTCString()
+            let unix = new Date(Number(date)).getTime()
+            console.log(unix,utc)
+            res.json({unix:unix,utc:utc})
+        }
     }
 })
 
