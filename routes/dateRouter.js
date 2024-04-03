60cf1a06c33241610fcdbc084155d40e3338a741
Steven Zheng <szheng.persevere@gmail.com>
2024-04-03 10:29:03 -0500

    roughly 4 tests are failing trying again

--- routes/dateRouter.js
+++ routes/dateRouter.js
@@ -3,14 +3,17 @@ const router = express.Router()
 
 router.get("/:date?",(req,res)=>{
     const {date}=req.params
+    console.log(date)
     if(!date){
         var newDate = new Date()
         let unix = Date.now()
         let utc = newDate.toUTCString()
+        console.log(unix,utc)
         res.json({unix:unix,utc:utc})
     } else {
         let utc = new Date(Number(date)).toUTCString()
         let unix = new Date(Number(date)).getTime()
+        console.log(unix,utc)
         res.json({unix:unix,utc:utc})
     }
 })
